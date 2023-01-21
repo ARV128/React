b09890fce39266f32598808e05d997ae39325caa
ARV128 <angelinavukovic1999@gmail.com>
2023-01-21 12:08:55 +0100

    omoguceno sortiranje po ceni

--- src/Pocetna.jsx
+++ src/Pocetna.jsx
@@ -3,16 +3,40 @@ import PutovanjeCard from "./PutovanjeCard";
 
  
 function Pocetna({putovanja}) {
-  
+    const [sort, setSort] = useState(true);
+    function sortAsc(){
+    
+        setSort(true);
+      }
+      function sortDesc(){
+        
+        setSort(false);
+      }
     return (
-       
-        <div className="svaPutovanja">
-                {putovanja.map((p)=><PutovanjeCard putovanje={p} key={p.id}></PutovanjeCard>)}
-        </div>
+        <>
+            <div className="buttons">
+                <button className="btn primary" onClick={sortAsc}>Sortiraj rastuće</button>
+                <button className="btn primary" onClick={sortDesc}>Sortiraj opadajuće</button>
+            </div>
+            <div className="svaPutovanja">
+                    {
+                     sort === true ? 
+                    
+                        putovanja
+                        .sort((a, b) => a.cena < b.cena ? -1 : 1)
+                        .map((p)=><PutovanjeCard putovanje={p} key={p.id}></PutovanjeCard>)
+                    :
+                        putovanja
+                        .sort((a, b) => a.cena > b.cena ? -1 : 1)
+                        .map((p)=><PutovanjeCard putovanje={p} key={p.id}></PutovanjeCard>)
+                
+                
+                }
+            </div>
                  
            
       
-      
+        </>
     );
   }
   
